8f91f8ebe7676f48e03ad723e45db59d11a76a5e
rElasri <rayanpro54@gmail.com>
2026-09-25 09:03:24 +0200

    Ajout du code dans la classe Identite

--- src/classes/Identite.java
+++ src/classes/Identite.java
@@ -1,4 +1,24 @@
 package classes;
 
 public class Identite {
+    private int nip;
+    private String nom,prenom;
+
+    public Identite(int num,String n,String p){
+        this.nip = num;
+        this.nom = n;
+        this.prenom = p;
+    }
+
+    public int getNip(){
+        return this.nip;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getPrenom(){
+        return this.prenom;
+    }
 }
